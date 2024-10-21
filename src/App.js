f2367c4871ba5f3a0478a0f537358941c8c55ef6
xShir0s <mtodys2006@gmail.com>
2024-10-21 11:05:13 +0200

    dodanie innych obrazów i opisów

--- src/App.js
+++ src/App.js
@@ -38,14 +38,12 @@ function App() {
 
           <div className="container">
               <div className="imgContainer">
-                  <img src="https://www.stylowy.net/data/film/35/3599_500x720.jpg"/>
+                  <img src="https://www.stylowy.net/data/film/35/3592_500x720.jpg"/>
               </div>
               <div className="text">
-                  <h1>DZIKI ROBOT</h1>
-                  <p className="normal">animowany/przygodowy/familijny</p>
-                  <p className="hint">„Dziki robot” to nowa animacja ze studia Dreamworks. Film jest realizowany na
-                      podstawie bestsellera New York Timesa, autorstwa Petera Browna. To historia robota ROZZUM, w
-                      skrócie "Roz"...</p>
+                  <h1>MÓJ PRZYJACIEL PINGWIN</h1>
+                  <p className="normal">familijny</p>
+                  <p className="hint">„Inspirowana prawdziwymi wydarzeniami historia niezwykłej przyjaźni między człowiekiem i małym pingwinkiem, to propozycja dla widzów w każdym wieku. Mądra, piękna historia, która poruszyła serca...</p>
               </div>
 
               <div className="footer">
@@ -54,14 +52,12 @@ function App() {
           </div>
           <div className="container">
               <div className="imgContainer">
-                  <img src="https://www.stylowy.net/data/film/35/3599_500x720.jpg"/>
+                  <img src="https://www.stylowy.net/data/film/36/3603_500x720.jpg"/>
               </div>
               <div className="text">
-                  <h1>DZIKI ROBOT</h1>
-                  <p className="normal">animowany/przygodowy/familijny</p>
-                  <p className="hint">„Dziki robot” to nowa animacja ze studia Dreamworks. Film jest realizowany na
-                      podstawie bestsellera New York Timesa, autorstwa Petera Browna. To historia robota ROZZUM, w
-                      skrócie "Roz"...</p>
+                  <h1>TERRIFIER 3</h1>
+                  <p className="normal">horror</p>
+                  <p className="hint">Krwawy filmowy fenomen ostatnich lat powraca, a wraz z nim psychopatyczny morderca w przebraniu klauna. To nie będzie cicha noc w Miles County. Krzyki ofiar przerwą ciszę, a krew zabarwi śnieg. W...</p>
               </div>
 
               <div className="footer">
@@ -71,11 +67,12 @@ function App() {
 
           <div className="container">
               <div className="imgContainer">
-                  <img src="https://www.stylowy.net/data/film/35/3599_500x720.jpg"/>
+                  <img src="https://www.stylowy.net/data/film/35/3598_500x720.jpg"/>
               </div>
               <div className="text">
-                  <h1>DZIKI ROBOT</h1>
-                  <p className="normal">animowany/przygodowy/familijny</p>
+                  <h1>
+                      UŚMIECHNIJ SIĘ 2</h1>
+                  <p className="normal">horror/thriller</p>
                   <p className="hint">„Dziki robot” to nowa animacja ze studia Dreamworks. Film jest realizowany na
                       podstawie bestsellera New York Timesa, autorstwa Petera Browna. To historia robota ROZZUM, w
                       skrócie "Roz"...</p>
@@ -88,14 +85,12 @@ function App() {
 
           <div className="container">
               <div className="imgContainer">
-                  <img src="https://www.stylowy.net/data/film/35/3599_500x720.jpg"/>
+                  <img src="https://www.stylowy.net/data/film/35/3597_500x720.jpg"/>
               </div>
               <div className="text">
-                  <h1>DZIKI ROBOT</h1>
-                  <p className="normal">animowany/przygodowy/familijny</p>
-                  <p className="hint">„Dziki robot” to nowa animacja ze studia Dreamworks. Film jest realizowany na
-                      podstawie bestsellera New York Timesa, autorstwa Petera Browna. To historia robota ROZZUM, w
-                      skrócie "Roz"...</p>
+                  <h1>WROOKLYN ZOO</h1>
+                  <p className="normal">dramat</p>
+                  <p className="hint">Osiemnastoletni Kosa jest najlepszym skejterem we Wrocławiu. Ma piękną dziewczynę, świetnych kumpli, uwielbianego dziadka (Jan Frycz) i jasny plan na przyszłość. Chce wygrać prestiżowe deskorolkowe...</p>
               </div>
 
               <div className="footer">
